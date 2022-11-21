8a492e39af413d46b8200c60f1018d7201d74f87
Hassan Ali <7assan.3li1998@gmail.com>
2022-11-21 02:24:06 +0200

    update api url

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "https://portfolio-dashboard-api.onrender.com";
+axios.defaults.baseURL = "https://api.hassanali.tk";
+// axios.defaults.baseURL = "https://portfolio-dashboard-api.onrender.com";
 // axios.defaults.baseURL = "http://localhost:4001";
 
 export default axios;
